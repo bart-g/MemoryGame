929a9372b809aa3fbf12a53a19dda9ce73a48987
Bartosz <bartoszwgorka@gmail.com>
2021-08-26 16:36:13 +0200

    Initial commit

---
+++ MemoryGame/MemoryGame/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MemoryGame
+//
+//  Created by Bartosz Górka on 26/08/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = LobbyViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
